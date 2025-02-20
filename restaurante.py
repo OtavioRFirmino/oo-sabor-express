571e1b88c0144b96c2655fe09f5eb1928d4d46f9
OtavioRFirmino <tavinho.renato@gmail.com>
2025-02-20 16:38:59 -0300

    Atualização.

--- restaurante.py
+++ restaurante.py
@@ -1,19 +1,25 @@
 class Restaurante:
     restaurantes = []
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
-        return f'{self.nome} -> {self.categoria}'
+        return f'{self._nome} -> {self._categoria}'
 
     def listar_restaurantes():
+        print(f'{'_nome:'.ljust(20)} | {'Categoria:'.ljust(20)} | {'Status:'.ljust(20)}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(20)} | {restaurante._categoria.ljust(20)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+     return '☑' if self._ativo else '☐'
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 Restaurante.listar_restaurantes()
+
