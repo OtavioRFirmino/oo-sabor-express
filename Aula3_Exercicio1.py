2c9f7ea05bd6647d1cc9c4605d9ad2d7c5c8f0d2
OtavioRFirmino <tavinho.renato@gmail.com>
2025-02-21 14:43:20 -0300

    Adição do exercicio 2 aula 3

--- Aula3_Exercicio1.py
+++ Aula3_Exercicio1.py
@@ -8,5 +8,37 @@ class Pessoa:
 
   def __str__(self):
     return f'Nome: {self.nome}\nIdade: {self.idade}\nProfissao: {self.profissao}'
+  
+  def aniversario(self):
+    self.idade += 1
+  
+  @property
+  def saudacao(self):
+    return f'Bem vindo a vaga de {self.profissao}!'
+
 pessoa_otavio = Pessoa('Otávio',22,'Suporte')
-print(pessoa_otavio)
+pessoa_otavio.aniversario()
+
+print(pessoa_otavio.saudacao)
+
+#Resposta
+
+"""class Pessoa:
+    def __init__(self, nome='', idade=0, profissao=''):
+        self.nome = nome
+        self.idade = idade
+        self.profissao = profissao
+
+    def __str__(self):
+        return f'{self.nome}, {self.idade} anos, {self.profissao}'
+
+    @property
+    def saudacao(self):
+        if self.profissao:
+            return f'Olá, sou {self.nome}! Trabalho como {self.profissao}.'
+        else:
+            return f'Olá, sou {self.nome}!'
+
+    def aniversario(self):
+        self.idade += 1
+"""
